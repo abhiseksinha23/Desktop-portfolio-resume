fb9127271eb1ef38f85693f1b7173b28e49a758f
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2019-04-02 00:31:47 +0530

    Add files via upload

---
+++ Resume/js/common.js
@@ -0,0 +1,37 @@
+jQuery(function ($) {
+
+    'use strict';
+	
+
+new WOW().init();
+
+
+jQuery(window).bind('load', function () {
+	parallaxInit();						  
+});
+function parallaxInit() {
+    jQuery('.parallax').each(function(){
+        jQuery(this).parallax("30%", 0.1);
+    });
+}
+
+
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -40
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+});
